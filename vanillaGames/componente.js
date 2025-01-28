74c54bf1624c782bf16d5d499473551682d08239
didacfrndz <156692497+didacfrndz@users.noreply.github.com>
2025-01-28 14:58:50 +0000

    pruebas

--- vanillaGames/componente.js
+++
@@ -1,7 +0,0 @@
-export const componente = {
-    template: `Código html del componente`,
-    string: ()=>{
-      //logica javascript para el componente
-    } 
-  }
-  
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'assets',
+server:{
+  /* port: 8080, */ // Podemos definir el puerto de salida
+  hot: true
+}
 })
+
+
+  
+
